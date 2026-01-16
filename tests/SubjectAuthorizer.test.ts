e883163cf039836c9ecac3c7d934e38e29f091eb
Ethan Smith <ethan@onebytegone.com>
2026-01-15 23:48:01 -0500

    build: lint ts files

--- tests/SubjectAuthorizer.test.ts
+++ tests/SubjectAuthorizer.test.ts
@@ -4,21 +4,21 @@ import { ADMINISTER_OWN_AUTH, ADMINISTER_OTHER_AUTH, DO_NEARLY_EVERYTHING } from
 
 
 describe('SubjectAuthorizer', () => {
-   const ALL_ROLES_1 = [
+   const allRoles1 = [
       ADMINISTER_OWN_AUTH,
       ADMINISTER_OTHER_AUTH,
    ];
 
-   const ALL_ROLES_2 = [
+   const allRoles2 = [
       ADMINISTER_OWN_AUTH,
       ADMINISTER_OTHER_AUTH,
       DO_NEARLY_EVERYTHING,
    ];
 
-   const USER_ID = '73885b55-2e0d-40bd-8cb3-2e59cf78ed87';
+   const userID = '73885b55-2e0d-40bd-8cb3-2e59cf78ed87';
 
-   const USER: Claims = {
-      subjectID: USER_ID,
+   const user: Claims = {
+      subjectID: userID,
       roles: [
          { roleID: ADMINISTER_OWN_AUTH.roleID },
          { roleID: ADMINISTER_OTHER_AUTH.roleID, contextValue: '8e0fa760-9a1d-43ea-8686-768318d923b4' },
@@ -26,17 +26,17 @@ describe('SubjectAuthorizer', () => {
       ],
    };
 
-   const factory1 = new AuthorizerFactory(ALL_ROLES_1),
-         factory2 = new AuthorizerFactory(ALL_ROLES_2);
+   const factory1 = new AuthorizerFactory(allRoles1),
+         factory2 = new AuthorizerFactory(allRoles2);
 
    it('ignores unknown roles when no opts provided', () => {
-      expect(factory1.makeAuthorizerForSubject(USER)).to.be.ok; // eslint-disable-line no-unused-expressions
-      expect(factory2.makeAuthorizerForSubject(USER)).to.be.ok; // eslint-disable-line no-unused-expressions
+      expect(factory1.makeAuthorizerForSubject(user)).to.be.ok; // eslint-disable-line no-unused-expressions
+      expect(factory2.makeAuthorizerForSubject(user)).to.be.ok; // eslint-disable-line no-unused-expressions
    });
 
    it('throws an error on unknown roles when requested', () => {
-      expect(() => { factory1.makeAuthorizerForSubject(USER, { throwOnUnknownRole: true }); }).to.throw();
-      expect(factory2.makeAuthorizerForSubject(USER, { throwOnUnknownRole: true })).to.be.ok; // eslint-disable-line no-unused-expressions
+      expect(() => { factory1.makeAuthorizerForSubject(user, { throwOnUnknownRole: true }); }).to.throw();
+      expect(factory2.makeAuthorizerForSubject(user, { throwOnUnknownRole: true })).to.be.ok; // eslint-disable-line no-unused-expressions
    });
 
 });
